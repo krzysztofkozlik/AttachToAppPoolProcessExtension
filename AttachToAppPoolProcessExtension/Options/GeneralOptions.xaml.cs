683fa193bb563bb98218fcd23dcd316c12ef6b49
Krzysztof Koźlik (KLF) <KLF@kmd.dk>
2023-05-02 12:49:41 +0200

    Fixed auto width of List View with processes

--- AttachToAppPoolProcessExtension/Options/GeneralOptions.xaml.cs
+++ AttachToAppPoolProcessExtension/Options/GeneralOptions.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Data.Common;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace AttachToAppPoolProcessExtension.Options
@@ -16,6 +17,23 @@ namespace AttachToAppPoolProcessExtension.Options
 
             // Dummy call to force the loading of Microsoft.Xaml.Behaviors
             var _ = new Microsoft.Xaml.Behaviors.DefaultTriggerAttribute(typeof(Trigger), typeof(Microsoft.Xaml.Behaviors.TriggerBase), null);
+        }
+
+        private void ListViewTargetUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+            var lw = sender as System.Windows.Controls.ListView;
+
+            var gw = lw.View as GridView;
+
+            foreach (var column in gw.Columns)
+            {
+                if (double.IsNaN(column.Width))
+                {
+                    column.Width = column.ActualWidth;
+                }
+
+                //column.Width = double.NaN;
+            }
         }
     }
 }
