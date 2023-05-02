01530a9980bdc882f4e542e19621b12f5947c8a7
Krzysztof Koźlik (KLF) <KLF@kmd.dk>
2023-05-02 12:53:25 +0200

    Removed obsolete code

--- AttachToAppPoolProcessExtension/Options/GeneralOptions.xaml.cs
+++ AttachToAppPoolProcessExtension/Options/GeneralOptions.xaml.cs
@@ -18,22 +18,5 @@ namespace AttachToAppPoolProcessExtension.Options
             // Dummy call to force the loading of Microsoft.Xaml.Behaviors
             var _ = new Microsoft.Xaml.Behaviors.DefaultTriggerAttribute(typeof(Trigger), typeof(Microsoft.Xaml.Behaviors.TriggerBase), null);
         }
-
-        private void ListViewTargetUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
-        {
-            var lw = sender as System.Windows.Controls.ListView;
-
-            var gw = lw.View as GridView;
-
-            foreach (var column in gw.Columns)
-            {
-                if (double.IsNaN(column.Width))
-                {
-                    column.Width = column.ActualWidth;
-                }
-
-                //column.Width = double.NaN;
-            }
-        }
     }
 }
