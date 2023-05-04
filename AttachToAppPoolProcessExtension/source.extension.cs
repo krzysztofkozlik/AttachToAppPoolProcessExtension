9e7c09ef771fe55307327513cf0e81ddce22185a
Krzysztof Koźlik (KLF) <KLF@kmd.dk>
2023-05-04 11:39:12 +0200

    Added license, tags and link to repo

--- AttachToAppPoolProcessExtension/source.extension.cs
+++ AttachToAppPoolProcessExtension/source.extension.cs
@@ -8,11 +8,11 @@ namespace AttachToAppPoolProcessExtension
     internal sealed partial class Vsix
     {
         public const string Id = "AttachToAppPoolProcessExtension.3f4faf03-896c-4996-a482-a02eabc4cdde";
-        public const string Name = "Attach To App Pool Process";
-        public const string Description = @"Extension providing an easy way to attach to predefined IIS processes based the name of the App Pool they belong to.";
+        public const string Name = "Attach To App Pool Process Toolbar";
+        public const string Description = @"Toolbar providing an easy way to attach to predefined IIS processes based on the name of the associated App Pool.";
         public const string Language = "en-US";
         public const string Version = "1.0";
-        public const string Author = "KMD A/S";
-        public const string Tags = "";
+        public const string Author = "Krzysztof Koźlik";
+        public const string Tags = "Attach, Attach to process, Attach toolbar, Debug, Debugger, Debugging, Menus and Toolbars, IIS, App Pool";
     }
 }
